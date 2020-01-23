ea5ad2aff3cf280e77b0f2469ed18c0c53aefa60
Umair Idris <umairidris@google.com>
2020-01-23 17:33:29 -0500

    Initial implementation of namespace (#10)
    * initial implementation of namespace
    
    * add new line

--- provider.go
+++ provider.go
@@ -43,6 +43,7 @@ func Provider() *schema.Provider {
 			"cdap_application":       resourceApplication(),
 			"cdap_artifact":          resourceArtifact(),
 			"cdap_artifact_property": resourceArtifactProperty(),
+			"cdap_namespace":         resourceNamespace(),
 		},
 	}
 }

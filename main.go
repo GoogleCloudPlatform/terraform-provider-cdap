c13a5d0d5747d8aa87f70d63edfba72a8bfcbbaa
Umair Idris <umairidris@google.com>
2020-01-24 11:54:09 -0500

    move provider files into cdap package (#14)

--- main.go
+++ main.go
@@ -16,6 +16,8 @@
 package main
 
 import (
+	"terraform-provider-cdap/cdap"
+
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
@@ -23,7 +25,7 @@ import (
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return Provider()
+			return cdap.Provider()
 		},
 	})
 }

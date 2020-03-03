8f5c556440d5fe3a8fa7276cf4f43351b81dcc9d
Jacob Ferriero <jferriero@google.com>
2020-03-02 17:36:39 -0800

    rename to program run

--- cdap/resource_streaming_program_run.go
+++ cdap/resource_streaming_program_run.go
@@ -18,21 +18,26 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+    "strconv"
 	"log"
 	"net/http"
 	"time"
 
+    "github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
+// This is a special key terraform will use to check for the existence of this run related to a particular resource
+const TF_FAUX_RUN_ID string = "__TF_RUN_ID__"
+
 // https://docs.cdap.io/cdap/current/en/reference-manual/http-restful-api/lifecycle.html.
-func resourceStreamingProgram() *schema.Resource {
+func resourceStreamingProgramRun() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceStreamingProgramCreate,
-		Read:   resourceStreamingProgramRead,
-		Delete: resourceStreamingProgramDelete,
-		Exists: resourceStreamingProgramExists,
+		Create: resourceStreamingProgramRunCreate,
+		Read:   resourceStreamingProgramRunRead,
+		Delete: resourceStreamingProgramRunDelete,
+		Exists: resourceStreamingProgramRunExists,
 
 		Schema: map[string]*schema.Schema{
 			"namespace": {
@@ -80,7 +85,7 @@ func resourceStreamingProgram() *schema.Resource {
 	}
 }
 
-func resourceStreamingProgramCreate(d *schema.ResourceData, m interface{}) error {
+func resourceStreamingProgramRunCreate(d *schema.ResourceData, m interface{}) error {
 	config := m.(*Config)
 	name := d.Get("name").(string)
 
@@ -89,12 +94,18 @@ func resourceStreamingProgramCreate(d *schema.ResourceData, m interface{}) error
 		"/v3/namespaces", d.Get("namespace").(string),
 		"/apps", d.Get("app").(string), d.Get("type").(string),
 		name)
-	startAddr := urlJoin(
-		addr, "start")
-	statusAddr := urlJoin(
-		addr, "status")
+	startAddr := urlJoin(addr, "start")
+	statusAddr := urlJoin(addr, "status")
 
-	argsObj := make(map[string]interface{})
+    argsObj := make(map[string]string)
+    // cast map[string]interface{} to map[string]string
+    for k, val := range d.Get("runtime_arguments").(map[string]interface{}){
+        argsObj[k] = val.(string)
+    }
+
+    runId, _ := uuid.NewRandom()
+    // This runtime arg will be unused by the pipeline but will allow the provider to associate a run with this resource.
+    argsObj[TF_FAUX_RUN_ID] = runId.String()
 
 	b, err := json.Marshal(argsObj)
 	if err != nil {
@@ -129,7 +140,7 @@ func resourceStreamingProgramCreate(d *schema.ResourceData, m interface{}) error
 			time.Sleep(10 * time.Second)
 		case "RUNNING":
 			log.Println("program successfully reached RUNNING state.")
-			d.SetId(d.Get("app").(string))
+			d.SetId(runId.String())
 			return nil
 		case "FAILED":
 			return fmt.Errorf("failed to start program in app: %s in state: %s.", d.Get("app"), p.Status)
@@ -142,8 +153,58 @@ func resourceStreamingProgramCreate(d *schema.ResourceData, m interface{}) error
 	return nil
 }
 
-func resourceStreamingProgramRead(d *schema.ResourceData, m interface{}) error {
+func resourceStreamingProgramRunRead(d *schema.ResourceData, m interface{}) error {
 	return nil
+}
+
+// Checks if there is a running run for the terraform faux run id
+func isFauxRunIdRunning(config *Config, runsAddr string, runId string) (bool, error){
+	req, err := http.NewRequest(http.MethodGet, runsAddr, nil)
+	if err != nil {
+		return false, err
+	}
+
+	b, err := httpCall(config.httpClient, req)
+	if err != nil {
+		return false, err
+	}
+
+    // TODO better way to define this nested struct?
+    type RuntimeArgs struct {
+        FauxRunId string `json:TF_FAUX_RUN_ID`
+    }
+
+    type RuntimeProperties struct {
+        RuntimeArgs string `json:"runtimeArgs"`
+    }
+
+    // These are the only keys we need
+    type Run struct {
+        RunId string `json:"runid"`
+        Status string `json:"status"`
+        Properties RuntimeProperties
+    }
+
+    var runs []Run
+
+	if err := json.Unmarshal(b, &runs); err != nil {
+		return false, err
+	}
+
+    var args RuntimeArgs
+
+    for _, r := range(runs) {
+        // Unescaping runtime Args which are stored as an escaped JSON string.
+        unquotedArgs, _ := strconv.Unquote(r.Properties.RuntimeArgs)
+        b := []byte(unquotedArgs)
+        if err := json.Unmarshal(b, &args); err != nil {
+            return false, err
+        }
+        if (runId == args.FauxRunId) {
+            return true, nil
+        }
+    }
+    return false, nil
 }
 
 func getProgramStatus(config *Config, statusAddr string) (p ProgramStatus, err error) {
@@ -173,7 +234,7 @@ func stopProgram(config *Config, stopAddr string) error {
 	return err
 }
 
-func resourceStreamingProgramDelete(d *schema.ResourceData, m interface{}) error {
+func resourceStreamingProgramRunDelete(d *schema.ResourceData, m interface{}) error {
 	config := m.(*Config)
 	name := d.Get("name").(string)
 
@@ -213,16 +274,18 @@ func resourceStreamingProgramDelete(d *schema.ResourceData, m interface{}) error
 	}
 }
 
-func resourceStreamingProgramExists(d *schema.ResourceData, m interface{}) (bool, error) {
+func resourceStreamingProgramRunExists(d *schema.ResourceData, m interface{}) (bool, error) {
 	config := m.(*Config)
 
 	addr := urlJoin(
 		config.host,
 		"/v3/namespaces", d.Get("namespace").(string),
 		"/apps", d.Get("app").(string), d.Get("type").(string),
-		d.Get("name").(string), "/status")
+		d.Get("name").(string))
+	statusAddr := urlJoin(addr, "/status")
+	runAddr := urlJoin(addr, "/runs")
 
-	req, err := http.NewRequest(http.MethodGet, addr, nil)
+	req, err := http.NewRequest(http.MethodGet, statusAddr, nil)
 	if err != nil {
 		return false, err
 	}
@@ -237,9 +300,21 @@ func resourceStreamingProgramExists(d *schema.ResourceData, m interface{}) (bool
 		return false, err
 	}
 
+    // This checks if there program is running (but it may be running several times)
 	if p.Status == "RUNNING" {
-		return true, nil
+        // This handles ambiguity if there are multiple program runs
+        running, err := isFauxRunIdRunning(config, runAddr, d.Id())
+        if err != nil {
+            return false, err
+        }
+
+        if running {
+            return true, nil
+        }
+
+        return false, nil
 	}
+
 	return false, nil
 }
 

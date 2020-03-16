0da21a8f44f3318128a99e80a9d47c9fb9caee83
umairidris <umairidris@google.com>
2020-03-16 18:08:04 -0400

    add build helper script

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Usage:
+#   ./build.sh -o /tmp/cdap_provider -v v1.0.0
+
+set -e
+
+OUTPUT_DIR="."
+VERSION=""
+
+while getopts 'o:v:' c
+do
+  case $c in
+    o) OUTPUT_DIR=$OPTARG ;;
+    v) VERSION=$OPTARG ;;
+  esac
+done
+
+if [[ -z ${VERSION} ]]; then
+    echo "-v must be set"
+    exit 1
+fi
+
+platforms=("linux/amd64" "darwin/amd64" "windows/amd64")
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+
+    output_name="terraform-cdap-provider_${VERSION}_${GOOS}-${GOARCH}"
+    echo "Building ${output_name}"
+
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o ${OUTPUT_DIR}/${output_name}
+done

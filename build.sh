8eb984f717773da624232d377036a8048e75565b
umairidris <umairidris@google.com>
2020-03-18 17:43:08 -0400

    shellcheck

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
-# Usage:
-#   ./build.sh -o /tmp/cdap_provider -v v1.0.0
+#!/bin/bash
+# Usage: ./build.sh -o /tmp/cdap_provider -v v1.0.0
 
 set -e
 
@@ -10,8 +9,12 @@ VERSION=""
 while getopts 'o:v:' c
 do
   case $c in
-    o) OUTPUT_DIR=$OPTARG ;;
-    v) VERSION=$OPTARG ;;
+    o) OUTPUT_DIR=${OPTARG} ;;
+    v) VERSION=${OPTARG} ;;
+    *)
+      echo "Invalid flag ${OPTARG}"
+      exit 1
+      ;;
   esac
 done
 
@@ -24,12 +27,13 @@ platforms=("linux/amd64" "darwin/amd64" "windows/amd64")
 
 for platform in "${platforms[@]}"
 do
-    platform_split=(${platform//\// })
+    IFS="/"
+    read -ra platform_split <<< "${platform}"
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
 
     output_name="terraform-cdap-provider_${VERSION}_${GOOS}-${GOARCH}"
     echo "Building ${output_name}"
 
-    env GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${OUTPUT_DIR}/${output_name}
+    env GOOS="${GOOS}" GOARCH="${GOARCH}" go build -o "${OUTPUT_DIR}"/"${output_name}"
 done
